5498e4bb662c364bf698115c184a1604f885397b
abdelrahman <104737626+AbdAlrahmanmohamedelsayed@users.noreply.github.com>
2024-10-24 14:42:48 +0300

    edit onboard app

--- lib/modules/screens/onboard/onboard.dart
+++ lib/modules/screens/onboard/onboard.dart
@@ -1,10 +1,10 @@
 import 'package:animate_do/animate_do.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_onboarding_slider/flutter_onboarding_slider.dart';
+import 'package:lottie/lottie.dart';
 import 'package:todo_app/core/config/page_routes_name.dart';
 import 'package:todo_app/core/theme/color_pallets.dart';
-import 'package:todo_app/modules/screens/onboard/widgets/onboard_image.dart';
+
 import 'package:todo_app/modules/screens/onboard/widgets/onboard_text.dart';
 
 class Onboarding extends StatelessWidget {
@@ -40,25 +40,34 @@ class Onboarding extends StatelessWidget {
       headerBackgroundColor: ColorPallets.backGroundColorLight,
       pageBackgroundColor: ColorPallets.backGroundColorLight,
       centerBackground: true,
-      background: const [
-        OnboardImage(imagepath: 'assets/images/oboard_1.png'),
-        OnboardImage(imagepath: 'assets/images/oboard_2.png'),
-        OnboardImage(imagepath: 'assets/images/oboard_3.png'),
+      background: [
+        Lottie.asset(width: 350, 'assets/images/lottie/manage_task.json'),
+        Lottie.asset(width: 350, 'assets/images/lottie/onboard_2.json'),
+        Lottie.asset(width: 450, 'assets/images/lottie/onboard_3.json'),
       ],
       speed: 1.8,
-      pageBodies: const [
-        OnboardText(
-            tittle: 'Manage your tasks',
-            discrebtion:
-                'You can easily manage all of your daily tasks in DoMe for free'),
-        OnboardText(
-            tittle: 'Create daily routine',
-            discrebtion:
-                'In Uptodo  you can create your personalized routine to stay productive'),
-        OnboardText(
-            tittle: 'Orgonaize your tasks',
-            discrebtion:
-                'You can organize your daily tasks by adding your tasks into separate categories')
+      pageBodies: [
+        FadeInRight(
+          delay: const Duration(milliseconds: 500),
+          child: const OnboardText(
+              tittle: 'Manage your tasks',
+              discrebtion:
+                  'You can easily manage all of your daily tasks in DoMe for free'),
+        ),
+        FadeInRight(
+          delay: const Duration(milliseconds: 500),
+          child: const OnboardText(
+              tittle: 'Create daily routine',
+              discrebtion:
+                  'In Uptodo  you can create your personalized routine to stay productive'),
+        ),
+        FadeInRight(
+          delay: const Duration(milliseconds: 500),
+          child: const OnboardText(
+              tittle: 'Orgonaize your tasks',
+              discrebtion:
+                  'You can organize your daily tasks by adding your tasks into separate categories'),
+        )
       ],
     );
   }
